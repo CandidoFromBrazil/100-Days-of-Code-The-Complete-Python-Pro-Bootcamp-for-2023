87d1ce348494aef2cde58a4c162ab2b73af05fed
Matheus Willians Fernandes Candido <102835123+CandidoFromBrazil@users.noreply.github.com>
2022-11-29 14:12:29 -0300

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,3 +1,4 @@
+# Band name generator
 print("Hello lad, howdy ya?" + "Pleasure to have ya with we today")
 townName =(input("Where was your birth town, I mean, your hometown?\n"))
 pet =(input("Have you got yourself a pet in your childhood? what was it's name?\n"))
