e33674d844cd8a1843d647f8fcbd5e7588954d88
Matheus Willians Fernandes Candido <102835123+CandidoFromBrazil@users.noreply.github.com>
2022-11-29 18:49:32 -0300

    Update day2.py

--- day2.py
+++ day2.py
@@ -7,5 +7,5 @@ tip =int(input("The tip for the waitress will be of: 12%, 15% or 20%? "))
 percent_tip = tip/100
 to_pay = (bill/people) * percent_tip
 
-final_amount = round(to_pay,2)
+final_amount = "{:.2f}".format(to_pay)
 print(f"Each person must pay the amount of: $ {final_amount} to the waitress")
