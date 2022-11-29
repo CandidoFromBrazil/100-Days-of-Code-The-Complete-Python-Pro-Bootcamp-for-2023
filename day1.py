88c4eb053d680d86fb82630cbc9c39c92313e619
Matheus Willians Fernandes Candido <102835123+CandidoFromBrazil@users.noreply.github.com>
2022-11-29 14:13:43 -0300

    Update day1.py

--- day1.py
+++ day1.py
@@ -2,5 +2,4 @@
 print("Hello lad, howdy ya?" + "Pleasure to have ya with we today")
 townName =(input("Where was your birth town, I mean, your hometown?\n"))
 pet =(input("Have you got yourself a pet in your childhood? what was it's name?\n"))
-
 print("The real-fancy and perfect name for your brand-new band ought to be: " + townName + " " + pet + " Band")
