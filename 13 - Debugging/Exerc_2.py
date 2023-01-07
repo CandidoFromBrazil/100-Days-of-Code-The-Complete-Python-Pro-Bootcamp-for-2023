63d82f97a22ce773faf922e078e95123e35ee2d3
Matheus Willians Fernandes Candido <102835123+CandidoFromBrazil@users.noreply.github.com>
2023-01-07 06:31:45 -0300

    Add files via upload

---
+++ 13 - Debugging/Exerc_2.py
@@ -0,0 +1,30 @@
+#RAW:
+#year = input("Which year do you want to check?")
+
+#if year % 4 == 0:
+#  if year % 100 == 0:
+#    if year % 400 == 0:
+#      print("Leap year.")
+#    else:
+#      print("Not leap year.")
+#  else:
+#    print("Leap year.")
+#else:
+#  print("Not leap year.")
+  
+
+#DEBUGGED:
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+  
+  
